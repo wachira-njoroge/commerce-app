0e46f9d0d3482a182bb9a189ce5d0d72ea5d0c35
wachira-njoroge <solomonwachira212@gmail.com>
2025-02-09 06:24:55 +0300

    Created the database models

---
+++ sellerapp/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    phone = models.CharField(max_length=100)
+    region = models.CharField(max_length=100)
+    date_created = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.name 
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.FloatField()
+    category = models.CharField(max_length=100)
+    description = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    ref_no = models.CharField(max_length=100)    
+    date_created = models.DateTimeField(auto_now_add=True)
+    delivery_date = models.DateTimeField()
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='orders', null=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.ref_no
+

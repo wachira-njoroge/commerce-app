1a9752074d39737a9df473196fac129876dd26b5
wachira-njoroge <solomonwachira212@gmail.com>
2025-02-09 21:03:21 +0300

    Created restful api for creating products and customers

--- sellerapp/urls.py
+++ sellerapp/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
-from . import views
+from .views import(
+    CustomerCreateAPIView,
+    CustomerListAPIView,
+    ProductCreateAPIView,
+    CategoryCreateAPIView
+)
+
+# Added URL patterns for creating and listing customers
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('customer/create/', CustomerCreateAPIView.as_view(), name='customer-create'),
+    path('customer/list/', CustomerListAPIView.as_view(), name='customer-list'),
+    path('product/create/', ProductCreateAPIView.as_view(), name='product-create'),
+    path('category/create/', CategoryCreateAPIView.as_view(), name='category-create')
 ]

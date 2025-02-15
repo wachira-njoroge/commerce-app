077f5c9fbb21e6cfeb208024562c50a99bdf3b32
wachira-njoroge <solomonwachira212@gmail.com>
2025-02-15 17:22:42 +0300

    Added route to oauth redirect

--- eseller/urls.py
+++ eseller/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('sellerapp/', include('sellerapp.urls')),
+    path('auth/', include("oauth2_provider.urls", namespace="oauth2_provider")),
+    path("accounts/login/", auth_views.LoginView.as_view(), name="login"),
+    #path("auth/", include("rest_framework.urls")),
 ]

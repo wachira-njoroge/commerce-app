28b6bfd46c82134fa2625b83f6d771072d4a3b36
wachira-njoroge <solomonwachira212@gmail.com>
2025-02-15 17:24:44 +0300

    Added a callback function for auth redirects

--- sellerapp/views.py
+++ sellerapp/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import CustomerSerializer, ProductSerializer , CategorySerializer
 from .models import Customer, Product, Category, Order
+import requests
 
 class CustomerCreateAPIView(generics.CreateAPIView):
     queryset = Customer.objects.all()
@@ -42,10 +43,36 @@ class CategoryCreateAPIView(APIView):
 
         return Response({"message": "Categories and products created successfully"}, status=status.HTTP_201_CREATED)
 
+def CallbackView(request):
+    code = request.GET.get('code')
+    if not code:
+           return JsonResponse({"error": "Missing authorization code"}, status=400)
 
+    token_url = 'http://localhost:8000/auth/token/'
+    client_id = 'zZ8mnCqVK5oaxZGs0zYfu08nJHPCkLbA3AC9QnMe'
+    client_secret = 'pbkdf2_sha256$870000$QeTmagQSOqIWtfjBuxnsIc$WddVjlc6GDA/vaCApZyeJqwyaFyl8DQ+BYZG6eCCNBM='
+    redirect_uri = 'http://localhost:8000/auth/callback/'
+    data = {
+        'grant_type': 'authorization_code',
+        'code': code,
+        'redirect_uri': redirect_uri,
+        'client_id': client_id,
+        'client_secret': client_secret
+    }
 
+    response = requests.post(token_url, data=data)
+    if response.status_code != 200:
+        return JsonResponse({"error":"Failed to get token"}, status=response.status_code)
 
+    tokenAccess = response.json().get('access_token')
+    tokenID = response.json().get('id_token')
 
+    return JsonResponse({
+        'id_token': id_token,
+        'access_token': access_token,
+        'token_type': token_json.get('token_type'),
+        'expires_in': token_json.get('expires_in'),
+    })
 
 
 

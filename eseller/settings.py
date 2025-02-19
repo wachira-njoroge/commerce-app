118f8dbcdbb783317f1b2333c415f8b82e20cd33
wachira-njoroge <solomonwachira212@gmail.com>
2025-02-19 17:42:21 +0300

    how about using docker host internally

--- eseller/settings.py
+++ eseller/settings.py
@@ -142,7 +142,7 @@ DATABASES = {
         'NAME': 'sellerdb',
         'USER': 'merchant',
         'PASSWORD': 'sela00',
-        'HOST': '54.93.230.197',
+        'HOST': 'host.docker.internal',
         'PORT': '3306'
     }
 }

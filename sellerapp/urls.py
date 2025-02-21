b9d03fefba76f334b6ae2359717d55059c24b0d4
wachira-njoroge <solomonwachira212@gmail.com>
2025-02-21 06:18:22 +0300

    Added an orders create view

--- sellerapp/urls.py
+++ sellerapp/urls.py
@@ -3,7 +3,11 @@ from .views import(
     CustomerCreateAPIView,
     CustomerListAPIView,
     ProductCreateAPIView,
-    CategoryCreateAPIView
+    CategoryCreateAPIView,
+    CategoryListAPIView,
+    ProductListAPIView,
+    OrderCreateAPIView,
+    OrderListAPIView
 )
 
 # Added URL patterns for creating and listing customers
@@ -12,5 +16,9 @@ urlpatterns = [
     path('customer/create/', CustomerCreateAPIView.as_view(), name='customer-create'),
     path('customer/list/', CustomerListAPIView.as_view(), name='customer-list'),
     path('product/create/', ProductCreateAPIView.as_view(), name='product-create'),
-    path('category/create/', CategoryCreateAPIView.as_view(), name='category-create')
+    path('product/list/', ProductListAPIView.as_view(), name='product-list'),
+    path('category/create/', CategoryCreateAPIView.as_view(), name='category-create'),
+    path('category/list/', CategoryListAPIView.as_view(), name='category-list'),
+    path('order/create/', OrderCreateAPIView.as_view(), name='order-create'),
+    path('order/list/', OrderListAPIView.as_view(), name='order-list')
 ]
--- sellerapp/views.py
+++ sellerapp/views.py
@@ -3,26 +3,28 @@ from django.http import HttpResponse
 from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .serializers import CustomerSerializer, ProductSerializer , CategorySerializer
+from .serializers import CustomerSerializer, ProductSerializer , CategorySerializer, OrderSerializer
 from .models import Customer, Product, Category, Order
 import requests
 
 class CustomerCreateAPIView(generics.CreateAPIView):
-    queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+    http_method_names = ['post']
 
 class CustomerListAPIView(generics.ListAPIView):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
 
-# The below view is used to create a new product 
-class ProductCreateAPIView(generics.CreateAPIView):
+class ProductListAPIView(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
-class CategoryCreateAPIView(APIView):
-    # queryset = Category.objects.all()
-    # serializer_class = CategorySerializer
+# The below view is used to create a new product 
+class ProductCreateAPIView(generics.CreateAPIView):
+    serializer_class = ProductSerializer
+
+class CategoryCreateAPIView(generics.CreateAPIView):
+    serializer_class = CategorySerializer
     def post(self, request):
         data = request.data.get('categories', [])
 
@@ -43,36 +45,17 @@ class CategoryCreateAPIView(APIView):
 
         return Response({"message": "Categories and products created successfully"}, status=status.HTTP_201_CREATED)
 
-def CallbackView(request):
-    code = request.GET.get('code')
-    if not code:
-           return JsonResponse({"error": "Missing authorization code"}, status=400)
+class CategoryListAPIView(generics.ListAPIView):
+    queryset = Category.objects.all()   
+    serializer_class = CategorySerializer
 
-    token_url = 'http://localhost:8000/auth/token/'
-    client_id = 'zZ8mnCqVK5oaxZGs0zYfu08nJHPCkLbA3AC9QnMe'
-    client_secret = 'pbkdf2_sha256$870000$QeTmagQSOqIWtfjBuxnsIc$WddVjlc6GDA/vaCApZyeJqwyaFyl8DQ+BYZG6eCCNBM='
-    redirect_uri = 'http://localhost:8000/auth/callback/'
-    data = {
-        'grant_type': 'authorization_code',
-        'code': code,
-        'redirect_uri': redirect_uri,
-        'client_id': client_id,
-        'client_secret': client_secret
-    }
+class OrderCreateAPIView(generics.CreateAPIView):
+    serializer_class = OrderSerializer
+    http_method_names = ['post']
 
-    response = requests.post(token_url, data=data)
-    if response.status_code != 200:
-        return JsonResponse({"error":"Failed to get token"}, status=response.status_code)
-
-    tokenAccess = response.json().get('access_token')
-    tokenID = response.json().get('id_token')
-
-    return JsonResponse({
-        'id_token': id_token,
-        'access_token': access_token,
-        'token_type': token_json.get('token_type'),
-        'expires_in': token_json.get('expires_in'),
-    })
+class OrderListAPIView(generics.ListAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
 
 
 

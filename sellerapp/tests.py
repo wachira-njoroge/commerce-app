4242c75e3fd5c5439ad7c718b916fe6d4fb749ed
wachira-njoroge <solomonwachira212@gmail.com>
2025-02-21 10:00:58 +0300

    Added test cases

--- sellerapp/tests.py
+++ sellerapp/tests.py
@@ -1,3 +1,42 @@
-from django.test import TestCase
+import unittest
+import django
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eseller.settings')
+django.setup()
+from sellerapp.models import Customer, Category
+from django.urls import reverse
+from sellerapp.views import CustomerCreateAPIView, CategoryCreateAPIView
+from django.test import TestCase, Client
 
-# Create your tests here.
+class TestCases(TestCase):
+    def setUp(self):
+        self.client = Client()
+    
+    def test_customer_create(self):
+        response = self.client.post('customer-create', {
+            'name': 'Wafula',
+            'email': 'wafula@gmail.com'
+            })
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.data['name'], 'Wafula')
+
+    def test_category_create(self):
+        response = self.client.post('/category/create/', {
+            'categories': [
+                {
+                    'name': 'Furniture',
+                    'products': [
+                        {
+                            'name': 'Table',
+                            'price': 5000,
+                            'description': 'Coffee Table'
+                        }
+                    ]
+                }
+            ]
+        })
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.data['message'], 'Categories and products created successfully')
+
+        if __name__ == '__main__':
+            unittest.main()
